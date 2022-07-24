711c2bbe921bacecb8bfb77efe4c81be44cd3248
beko09 <abobakerhlal09@gmail.com>
2022-07-25 01:05:12 +0200

    edit:index.html => header+footer+slider

--- js/main.js
+++ js/main.js
@@ -3,12 +3,8 @@ window.onload = function () {
 
   const openNav = selectElement(".open-nav-mobile");
   const closeNav = selectElement(".close-mobile");
-  const openAside = selectElement(".open-aside");
-  const closeAside = selectElement(".close-aside");
   const menu = selectElement(".menu-mobile");
-  const aside = selectElement(".aside");
   const over = selectElement(".over");
-  const overAside = selectElement(".over-aside");
 
   if (openNav) {
     openNav.onclick = () => {
@@ -28,24 +24,7 @@ window.onload = function () {
       menu.classList.add("close-nav-mobile")
     };
   }
-  if (openAside) {
-    openAside.onclick = () => {
-      aside.classList.add("c-open-aside")
-      overAside.classList.add("overlay-aside")
-      aside.classList.remove("c-close-aside")
-    };
-    closeAside.onclick = () => {
-      aside.classList.remove("c-open-aside")
-      overAside.classList.remove("overlay-aside")
-      aside.classList.add("c-close-aside")
-    };
 
-    overAside.onclick = () => {
-      aside.classList.remove("c-open-aside")
-      overAside.classList.remove("overlay-aside")
-      aside.classList.add("c-close-aside")
-    };
-  }
 
 
   //  scroll to top
@@ -73,87 +52,60 @@ window.onload = function () {
   }
 
 
-  const accordionHeader = document.querySelectorAll(".accordion-header");
-  accordionHeader.forEach((header) => {
-    header.addEventListener("click", function () {
-      const accordionContent = header.parentElement.querySelector(".accordion-content");
-      let accordionMaxHeight = accordionContent.style.maxHeight;
-
-      // Condition handling
-      if (accordionMaxHeight == "0px" || accordionMaxHeight.length == 0) {
-        accordionContent.style.display = 'block';
-        accordionContent.style.maxHeight = `${accordionContent.scrollHeight + 32}px`;
-        header.querySelector(".fas").classList.remove("fa-plus");
-        header.querySelector(".fas").classList.add("fa-minus");
-        header.classList.add("bg-primary");
-        header.classList.add("text-white");
-        header.parentElement.classList.add("bg-white");
-      } else {
-        accordionContent.style.maxHeight = `0px`;
-        accordionContent.style.display = 'none';
-        header.querySelector(".fas").classList.add("fa-plus");
-        header.querySelector(".fas").classList.remove("fa-minus");
-        header.classList.remove("bg-primary");
-        header.classList.remove("text-white");
-        header.parentElement.classList.remove("bg-white");
-      }
-    });
-  });
 
 
   const customerClass = selectElement(".customer");
 
-  if (customerClass) {
-    //  sliders
-    const customer = new Swiper(".customer", {
-      slidesPerView: 3,
-      loop: true,
-      lazy: true,
-      grabCursor: true,
-      centeredSlides: true,
-      autoplay: {
-        delay: 1500,
-        disableOnInteraction: false,
-      },
 
-      breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-          centeredSlides: true,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-          centeredSlides: true,
-        },
-        1024: {
-          slidesPerView: 5,
-          spaceBetween: 20,
-          centeredSlides: true,
-        },
-      },
-    });
-    const intro = new Swiper(".intro-slider", {
-      slidesPerView: 1,
-      loop: true,
-      lazy: true,
-      effect: 'fade',
-      grabCursor: true,
-      centeredSlides: true,
-      autoplay: {
-        delay: 11000,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        type: "fraction",
-      },
-      navigation: {
-        nextEl: ".swiper-intro-next",
-        prevEl: ".swiper-intro-prev",
-      },
-    });
+  //  sliders
+  const customer = new Swiper(".customer", {
+    slidesPerView: 3,
+    loop: true,
+    lazy: true,
+    grabCursor: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },
 
-  }
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        centeredSlides: true,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        centeredSlides: true,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+        centeredSlides: true,
+      },
+    },
+  });
+  const intro = new Swiper(".intro-slider", {
+    slidesPerView: 1,
+    loop: true,
+    lazy: true,
+    effect: 'fade',
+    grabCursor: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-intro-next",
+      prevEl: ".swiper-intro-prev",
+    },
+  });
+
 }
